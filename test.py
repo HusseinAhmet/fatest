65db0651bbb6fd9ac5cf089c1af0987d3bbe558a
HusseinAhmet <90877802+HusseinAhmet@users.noreply.github.com>
2022-06-06 02:14:09 +0200

    tt

---
+++ test.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import json
+
+
+app= Flask(__name__)
+@app.route("/")
+def get_saw():
+  return "<h1>Welcome to Adaptive Speech</h1>"  
+@app.route("/get_yaw")
+def get_yaw():
+  return "<h1>Yaaaaaw</h1>"  
+@app.route("/hel")
+def maw():
+  return "<h1>Helloooooo</h1>"  
+
+
+app.run()
